f95c707d641b238e5cb9a7c56d85069cd516d74e
btc-200k <thaont.entertainment@gmail.com>
2024-02-23 15:36:14 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const cowsay = require("cowsay");
+
+const teaTypes = ['Green Tea', 'Chai Tea', 'Black Tea', 'Oolong Tea', 'Bubble Tea', 'Matcha Tea', 'Roibos Tea', 'White Tea', 'Herbal Tea', 'Purple Tea', 'Pu-erh Tea', 'Yellow Tea']
+
+function helloTea() {
+    let randomIndex = Math.floor(Math.random() * teaTypes.length);
+    console.log(cowsay.say({text:`A NEW DAY~ Give yourself a ${teaTypes[randomIndex]}`,f:'cowfee'}));
+}
+module.exports = { helloTea };
